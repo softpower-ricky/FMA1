618335aa81dddb787ab13506668c8ca9c07af88b
softpower-ricky <ricky@softpower.com.tw>
2016-11-11 09:56:18 +0800

    Add tiles setting

--- src/main/java/com/fma/web/home/HomeController.java
+++ src/main/java/com/fma/web/home/HomeController.java
@@ -1,25 +1,25 @@
-package com.fma.web;
+package com.fma.web.home;
 
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+@RequestMapping("home")
 @Controller
-@EnableAutoConfiguration
-public class BaseController {
+public class HomeController {
 
+   
     @RequestMapping("/")
     @ResponseBody
-    String home() {
-        return "Hello World";
+    ModelAndView test() {
+        return new ModelAndView("home");
     }
-    
+
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(BaseController.class, args);
+        SpringApplication.run(HomeController.class, args);
     }
 }
---
+++ src/main/webapp/WEB-INF/tiles/layout/basic.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+
+<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles"%>
+<html>
+<head>
+<title><tiles:getAsString name="title" /></title>
+</head>
+<body>
+	 Basic
+	 <tiles:insertAttribute name="header" />
+	 <tiles:insertAttribute name="body" />
+	 <tiles:insertAttribute name="footer" />
+</body>
+</html>
